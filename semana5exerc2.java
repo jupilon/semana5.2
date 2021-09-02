b802aff31e0e43be9fcff0c23effc28afab14310
jupilon <julianapilon2016@gmail.com>
2021-09-02 18:54:48 +0000

    exercicio2

---
+++ semana5exerc2.java
@@ -0,0 +1,16 @@
+public class semana5exerc2{
+    public static void main (String []args){
+        int x = 13;
+        while (x != 1) {
+         if (x % 2 == 0){
+            x= x / 2;
+        }
+        else{
+            x = 3 * x + 1;
+        }
+        System.out.println(x);
+
+        }
+
+    }
+}
